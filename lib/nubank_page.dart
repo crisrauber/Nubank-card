b479603819ce028c8376eec34ae65b6e5a888636
crisrauber <cristianaugusto1@hotmail.com>
2020-07-18 17:38:48 -0300

    adicionado TextField

--- lib/nubank_page.dart
+++ lib/nubank_page.dart
@@ -7,6 +7,19 @@ class NuBankPage extends StatefulWidget {
 }
 
 class _NuBankPageState extends State<NuBankPage> {
+  final nomeController = TextEditingController();
+  final numeroController = TextEditingController();
+
+  bool cardSide = true;
+
+  @override
+  void dispose() {
+    nomeController.dispose();
+    numeroController.dispose();
+    super.dispose();
+  }
+
+  GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,134 +32,174 @@ class _NuBankPageState extends State<NuBankPage> {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                labelStyle: TextStyle(color: Colors.white),
+                labelText: 'Nome do Titular'),
+            controller: nomeController,
+            onTap: () => {
+              if (!cardSide)
+                {
+                  setState(() {
+                    cardSide = !cardSide;
+                  }),
+                  cardKey.currentState.toggleCard()
+                },
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                labelStyle: TextStyle(color: Colors.white),
+                labelText: 'Número do Cartão'),
+            controller: numeroController,
+            onTap: () => {
+              if (cardSide)
+                {
+                  setState(() {
+                    cardSide = !cardSide;
+                  }),
+                  cardKey.currentState.toggleCard()
+                },
+            },
+          ),
           Container(
             child: Center(
               child: Container(
                 margin: const EdgeInsets.all(5),
-                width: 500,
-                height: 220,
-                child: FlipCard(
-                  direction: FlipDirection.VERTICAL,
-                  front: Container(
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      color: Color.fromRGBO(109, 33, 119, 1),
-                      child: Stack(
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Image.asset(
-                                'assets/mastercard.png',
-                                scale: 2.0,
-                              ),
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(16),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
+                child: AspectRatio(
+                  aspectRatio: 8 / 5,
+                  child: FlipCard(
+                    key: cardKey,
+                    direction: FlipDirection.VERTICAL,
+                    front: Container(
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        color: Color.fromRGBO(109, 33, 119, 1),
+                        child: Stack(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Image.asset(
-                                      'assets/chip.png',
-                                      scale: 15.0,
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(16),
-                                      child: Image.asset(
-                                        'assets/nfc.png',
-                                        scale: 150.0,
+                                Image.asset(
+                                  'assets/mastercard.png',
+                                  scale: 2.0,
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(16),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Image.asset(
+                                        'assets/chip.png',
+                                        scale: 15.0,
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(16),
+                                        child: Image.asset(
+                                          'assets/nfc.png',
+                                          scale: 150.0,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10, top: 90),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Image.asset(
+                                        'assets/nu_logo.png',
+                                        scale: 7.0,
                                         color: Colors.white,
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                      Padding(
+                                          padding: const EdgeInsets.all(16),
+                                          child: Text(
+                                            nomeController.text,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20),
+                                          )),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10, top: 90),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Image.asset(
-                                      'assets/nu_logo.png',
-                                      scale: 7.0,
-                                      color: Colors.white,
-                                    ),
-                                    Padding(
-                                        padding: const EdgeInsets.all(16),
-                                        child: Text(
-                                          'Cristian Augusto Rauber',
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 20),
-                                        )),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  back: Container(
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      color: Color.fromRGBO(109, 33, 119, 1),
-                      child: Stack(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                Container(
-                                  height: 80,
-                                  color: Color.fromRGBO(192, 192, 192, 1),
-                                )
-                              ],
+                    back: Container(
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        color: Color.fromRGBO(109, 33, 119, 1),
+                        child: Stack(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Container(
+                                    height: 80,
+                                    color: Color.fromRGBO(192, 192, 192, 1),
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10, top: 90),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Padding(
-                                        padding: const EdgeInsets.all(30),
-                                        child: Text(
-                                          '1234 5678 9101 1213',
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 20),
-                                        )),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 20),
-                                      child: Image.asset(
-                                        'assets/cirrus.png',
-                                        scale: 0.8,
-                                        // color: Colors.white,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ],
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10, top: 90),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Padding(
+                                          padding: const EdgeInsets.all(30),
+                                          child: Text(
+                                            numeroController.text,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20),
+                                          )),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 20),
+                                        child: Image.asset(
+                                          'assets/cirrus.png',
+                                          scale: 0.8,
+                                          // color: Colors.white,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
